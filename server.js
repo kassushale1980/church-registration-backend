e6bb6b7280a14993b545fbf8b20a38ac60d8c254
kassushale1980 <kassushale@gmail.com>
2026-07-06 19:57:17 -0400

    Update CORS and Render port

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ import memberRoutes from "./routes/members.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://churchkesisdani.netlify.app/"
+  ]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,8 +20,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/members", memberRoutes);
 app.use("/api/auth", authRoutes);
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
